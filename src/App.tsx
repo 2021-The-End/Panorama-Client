eab2857d8ffcd4e2a99a1d625d9a3e1272680778
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-05-24 20:16:41 +0900

    [Add] Panorama youtube video -4k-

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import React from "react";
 interface Props {}
 
 const App = (props: Props) => {
-  return <div>Panorama</div>;
+  return (
+    <iframe
+      width="1278"
+      height="719"
+      src="https://www.youtube.com/embed/7UJELUi1MVI"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
 };
 
 export default App;
