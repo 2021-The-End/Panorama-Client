1e97697f6ab0b23776e5e59200e9f4f37080970e
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-06-03 17:16:27 +0900

    [Update] Link to Scroll

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { PANORAMA } from "../../Assets";
 import * as S from "./Style";
+import { Link } from "react-scroll";
 
 const header = () => {
   return (
     <S.HeaderContainer>
       <ul>
-        <li>전시소개</li>
-        <li>작품소개</li>
+        <li>
+          <Link
+            activeClass="active"
+            to="panorama_info"
+            spy={true}
+            smooth={true}
+            duration={600}
+          >
+            전시소개
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="introduction_project"
+            spy={true}
+            smooth={true}
+            duration={600}
+          >
+            작품소개
+          </Link>
+        </li>
         <PANORAMA />
         <li>졸업생소개</li>
         <li>방명록</li>
