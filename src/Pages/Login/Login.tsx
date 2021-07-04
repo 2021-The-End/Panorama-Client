c0de7d95212f06bdf9769b9c9e67c4c986b0e08b
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-07-04 19:17:05 +0900

    [Update] Implement default logic with useState

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Header, Auth } from "../../Components";
 import * as S from "./Style";
 
 interface Props {}
 
 const Login = (props: Props) => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleLogin = () => {
+    console.log("id,pw :>> ", id, pw);
+  };
   return (
     <>
       <S.LoginPage>
         <Header attribute="auth" />
         <Auth headerText="로그인">
-          <input type="text" placeholder="ID" />
-          <input type="password" placeholder="Password" />
-          <div className="submit">
+          <input
+            type="text"
+            placeholder="ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={pw}
+            onChange={(e) => setPw(e.target.value)}
+          />
+          <div className="submit" onClick={handleLogin}>
             <span className="logintext">로그인</span>
             <span className="hidetext">Shot take a Panorama!</span>
           </div>
--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Header, Auth } from "../../Components";
 import * as S from "./Style";
 
 interface Props {}
 
 const Register = (props: Props) => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [conpw, setConpw] = useState("");
+
+  const handleLogin = () => {
+    if (pw === conpw) {
+      console.log("id,pw :>> ", id, pw);
+    } else {
+      console.log("비밀번호가 일치하지 않습니다.");
+    }
+  };
   return (
     <>
       <S.RegisterPage>
         <Header attribute="auth" />
         <Auth headerText="회원가입">
-          <input type="text" placeholder="ID"></input>
-          <input type="password" placeholder="Password"></input>
-          <input type="password" placeholder="Confirm Password"></input>
-          <div className="submit">회원가입</div>
+          <input
+            type="text"
+            placeholder="ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            value={pw}
+            onChange={(e) => setPw(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={conpw}
+            onChange={(e) => setConpw(e.target.value)}
+          ></input>
+          <div className="submit" onClick={handleLogin}>
+            회원가입
+          </div>
         </Auth>
       </S.RegisterPage>
       <Footer />
